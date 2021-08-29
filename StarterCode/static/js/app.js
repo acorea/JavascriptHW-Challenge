f1bde1e23a70f1064a2c6f5da122da37f5369981
AmeliaC <82566669+acorea@users.noreply.github.com>
2021-08-28 23:04:12 -0700

    Updated files

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,6 @@
-const samples_json = "https://github.com/acorea/plot.ly_challenge/blob/main/StarterCode/samples.json";
+// Step 1: Plotly
+// Use the D3 library to read in samples.json.
+const samples = "https://raw.githubusercontent.com/acorea/plot.ly_challenge/main/StarterCode/samples.json"
 
 var samples = ['a'];
 var metadata = [];
@@ -10,176 +12,63 @@ d3.json(samples_json).then(function(data) {
   init();
 });
 
-// Function for change on dropdown menu
-function optionChanged(selectedID){
 
-    // Check if value is selected in dropdown
-    console.log(selectedID);
- 
-   
-    // Clears dropdown
-    d3.select("#selDataset").html("");   
+function init() {
+    plotGraphsAndMetadata(samples[0], metadata[0])
+    dropdown = d3.select('#selDataset')
+    samples.forEach((value, i) =>
+        dropdown.append('option').attr('value',i).text(value.id)
+    )
+  }
+
+d3.selectAll("#selDataset").on("change", getData);
+
+function plotGraphsAndMetadata(sample, meta_sample) {
+
+    var bar_data = [{
+        x: sample.sample_values.slice(0,10),
+        y: sample.otu_ids.slice(0,10).reverse().map(x => 'OTU '+x),
+        text: sample.otu_labels.slice(0,10),
+        type: 'bar',
+        orientation: 'h'
+    }];
     
-    // Select the metadata array and for each item append the item ID and adds ID to dropdown
-    data.metadata.forEach(item =>
-         {
-          // console.log(item.id);
-         d3.select ("#selDataset").append('option').attr('value', item.id).text(item.id);
-         });
-    // Selected value is passed
-    d3.select("#selDataset").node().value = selectedID;
-    
-    // Filter Metadata for selected ID from dropdown
-    const idMetadata = data.metadata.filter(item=> (item.id == selectedID));
-       // {
-       //    console.log("------------------------")
-       //    console.log(item);
-       //    console.log(item.id);
-          
-       // });
-    // Check the metadata loaded for the selected ID
-    console.log(idMetadata);
-    
-    const panelDisplay = d3.select("#sample-metadata");
-    panelDisplay.html("");
-    Object.entries(idMetadata[0]).forEach(item=> 
-       {
-          // console.log(item);
-          panelDisplay.append("p").text(`${item[0]}: ${item[1]}`)
-       });
- 
-    // BAR CHART
- 
-    // Filter sample array data for the selected ID
-    const idSample = data.samples.filter(item => parseInt(item.id) == selectedID);
-    
-    // // Check values
-    // console.log(typeof parseInt(item.id));
-    // console.log(idSample[0].sample_values);  
-    // console.log(idSample[0].otu_ids);  
-    // console.log(idSample[0].otu_labels);  
-    
-    // Slice top 10 sample values
-    var sampleValue = idSample[0].sample_values.slice(0,10);
-    sampleValue= sampleValue.reverse();
-    var otuID = idSample[0].otu_ids.slice(0,10);
-    otuID = otuID.reverse();
-    var otuLabels = idSample[0].otu_labels
-    otuLabels = otuLabels.reverse();
- 
-    // // Check values
-    //  console.log(sampleValue);
-    //  console.log(otuID);
-    //  console.log(otuLabels);
- 
-    // Y axis of bar chart
-    const yAxis = otuID.map(item => 'OTU' + " " + item);
-       // console.log(yAxis);
-    
-    // Define the layout and trace object, edit color and orientation
-       const trace = {
-       y: yAxis,
-       x: sampleValue,
-       type: 'bar',
-       orientation: "h",
-       text:  otuLabels,
-       marker: {
-          color: 'rgb(154, 140, 152)',
-          line: {
-             width: 3
-         }
-        }
-       },
-       layout = {
-       title: 'Top 10 Operational Taxonomic Units (OTU)/Individual',
-       xaxis: {title: 'Number of Samples Collected'},
-       yaxis: {title: 'OTU ID'}
-       };
- 
-       // Plot using Plotly
-       Plotly.newPlot('bar', [trace], layout,  {responsive: true});    
-       
- // BUBBLE CHART
- 
- // Remove Sample value and otuID from individual
- var sampleValue1 =idSample[0].sample_values;
- var otuID1= idSample[0].otu_ids;
- 
- // Define the layout and trace object, edit color and orientation
- const trace1 = {
-    x: otuID1,
-    y: sampleValue1,
-    mode: 'markers',
-    marker: {
-      color: otuID1,
-      
-      size: sampleValue1
-    }
-  },
- 
-  layout1 = {
-    title: '<b>Bubble Chart For Each Sample</b>',
-    xaxis: {title: 'OTU ID'},
-    yaxis: {title: 'Number of Samples Collected'},
-    showlegend: false,
-    height: 800,
-    width: 1800
+    var bar_layout = {
+        //   height: 600,
+        //   width: 800
     };
-    
- // Plot using Plotly
- Plotly.newPlot('bubble', [trace1], layout1);
- 
- // BONUS: GAUGE CHART
 
- // Gauge Chart to plot weekly washing frequency 
- const guageDisplay = d3.select("#gauge");
- guageDisplay.html(""); 
- const washFreq = idMetadata[0].wfreq;
- 
- const guageData = [
-    {
-      domain: { x: [0, 1], y: [0, 1] },
-      value: washFreq,
-      title: { text: "<b>Belly Button Washing Frequency </b><br> (Scrubs Per Week)" },
-      type: "indicator",
-      mode: "gauge+number",     
-       gauge: {
-       axis: { range: [0,9] },
-       bar: { color: "#f2e9e4" },
-       steps: [
-          { range: [0, 1], color: "#e5d5d0" },
-          { range: [1, 2], color: "#dbc7c2" },
-          { range: [2, 3], color: "#d2b9b4" },
-          { range: [3, 4], color: "#c9ada7" },
-          { range: [4, 5], color: "#ac9899" },
-          { range: [5, 6], color: "#8a7e88" },
-          { range: [6, 7], color: "#7d7482" },
-          { range: [7, 8], color: "#706a7b" },
-          { range: [8, 9], color: "#4a4e69" }
-                
-        ],
-       threshold: {
-          value: washFreq
-        }
-      }
-    }
-  ]; 
-  const gaugeLayout = {  width: 600, 
-                   height: 400, 
-                   margin: { t: 0, b: 0 }, 
-                    };
- 
- // Plot using Plotly
-  Plotly.newPlot('gauge', guageData, gaugeLayout); 
- 
- });
- }
- 
- // Initial test starts at ID 940
- optionChanged(940);
- 
- // Event on change takes the value and calls the function during dropdown selection
- d3.select("#selDataset").on('change',() => {
- optionChanged(d3.event.target.value);
- 
- });
+    Plotly.newPlot('bar', bar_data, bar_layout);
+  
+    var bub_data = [{
+          y: sample.sample_values,
+          x: sample.otu_ids,
+          text: sample.otu_labels,
+          mode: 'markers',
+          marker: {
+              colorscale: 'Jet',
+              color: sample.otu_ids,
+              opacity: 0.9,
+              size: sample.sample_values
+          }
+    }];
+
+    var bub_layout = {
+        //   height: 600,
+        //   width: 800
+    };
+
+    Plotly.newPlot('bubble', bub_data, bub_layout);
+  
+    sample_metadata = d3.select('#sample-metadata');
+    sample_metadata.selectAll('p').remove()
+    for(key in meta_sample)
+          sample_metadata.append('p').text(`${key}:${meta_sample[key]}`);
+}
+
+function getData() {
+    var dropdownMenu = d3.select("#selDataset");
+    var i = dropdownMenu.property("value");
+    
+    plotGraphsAndMetadata(samples[i], metadata[i])
+}
