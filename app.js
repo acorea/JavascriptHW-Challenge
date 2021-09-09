c7e9c47477610321b1527973538c19c379b24e9d
AmeliaC <82566669+acorea@users.noreply.github.com>
2021-09-08 19:37:53 -0700

    moving files

--- app.js
+++ app.js
@@ -2,14 +2,15 @@
 // Use the D3 library to read in samples.json.
 const samples = "https://raw.githubusercontent.com/acorea/JavascriptHW-Challenge/main/Static/data/samples.json"
 
-var samples = ['a'];
-var metadata = [];
+let samples = ['a'];
+let metadata = [];
+
 // Fetch the JSON data and console log it
 d3.json(samples_json).then(function(data) {
-  samples = data.samples;
-  metadata = data.metadata;
-  console.log(data)
-  init();
+    samples = data.samples;
+    metadata = data.metadata;
+    console.log(data)
+    init();
 });
 
 //Update dropdown
@@ -17,7 +18,7 @@ function init() {
     plotGraphsAndMetadata(samples[0], metadata[0]);
     dropdown = d3.select('#selDataset');
     samples.forEach((value, i) =>
-        dropdown.append('option').attr('value',i).text(value.id)
+        dropdown.append('option').attr('value',`${i}`).text(`${value.id}`)
     )
   }
 
@@ -29,8 +30,7 @@ d3.selectAll("#selDataset").on("change", getData);
 //Use `otu_labels` as the hovertext for the chart.
 
 function plotGraphsAndMetadata(sample, meta_sample) {
-
-    var bar_data = [{
+    let bar_data = [{
         x: sample.sample_values.slice(0,10),
         y: sample.otu_ids.slice(0,10).reverse().map(x => 'OTU '+x),
         text: sample.otu_labels.slice(0,10),
@@ -38,9 +38,7 @@ function plotGraphsAndMetadata(sample, meta_sample) {
         orientation: 'h'
     }];
     
-    var bar_layout = {
-        //   height: 600,
-        //   width: 800
+    let bar_layout = {
     };
 
     Plotly.newPlot('bar', bar_data, bar_layout);
@@ -52,22 +50,20 @@ function plotGraphsAndMetadata(sample, meta_sample) {
 //Use `otu_ids` for the marker colors.
 //Use `otu_labels` for the text values.
 
-    var bub_data = [{
-          y: sample.sample_values,
-          x: sample.otu_ids,
-          text: sample.otu_labels,
-          mode: 'markers',
-          marker: {
-              colorscale: 'Jet',
-              color: sample.otu_ids,
-              opacity: 0.9,
-              size: sample.sample_values
+    let bub_data = [{
+        y: sample.sample_values,
+        x: sample.otu_ids,
+        text: sample.otu_labels,
+        mode: 'markers',
+        marker: {
+            colorscale: 'Jet',
+            color: sample.otu_ids,
+            opacity: 0.9,
+            size: sample.sample_values
           }
     }];
 
-    var bub_layout = {
-        //   height: 600,
-        //   width: 800
+    let bub_layout = {
     };
 
     Plotly.newPlot('bubble', bub_data, bub_layout);
@@ -75,12 +71,12 @@ function plotGraphsAndMetadata(sample, meta_sample) {
     sample_metadata = d3.select('#sample-metadata');
     sample_metadata.selectAll('p').remove();
     for(key in meta_sample)
-          sample_metadata.append('p').text(`${key}:${meta_sample[key]}`);
-}
+        sample_metadata.append('p').text(`${key}:${meta_sample[key]}`);
+    }
 
 function getData() {
-    var dropdownMenu = d3.select("#selDataset");
-    var i = dropdownMenu.property("value");
+    let dropdownMenu = d3.select("#selDataset");
+    let i = dropdownMenu.property("value");
     
     plotGraphsAndMetadata(samples[i], metadata[i])
 };
